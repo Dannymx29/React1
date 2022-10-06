be8bf16ce877b5429725305e9dc6d9727bbecb26
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-10-06 00:38:29 -0500

    inicio de carrito de ventas

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const initialState = {
+    cart: [],
+}
+
+const useInitialState = () => {
+    const [state, setState] = useState(initialState);
+
+    const AddToCart = (payload) => {
+        setState({
+            ...state,
+            cart:[...state.cart, payload]
+        })
+    }
+    return {
+        state, AddToCart
+    }
+}
+
+export default useInitialState;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,6 @@ const Login = () => {
 			username: formData.get('email'),
 			password: formData.get('password')
 		}
-		console.log(data);
 	}
 
 	return (
