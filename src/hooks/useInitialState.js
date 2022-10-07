4926c64fd2b0df69ea1e67b500efb485599d8526
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-10-07 00:59:41 -0500

    Eliminando productos del carrito

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -11,10 +11,20 @@ const useInitialState = () => {
         setState({
             ...state,
             cart:[...state.cart, payload]
-        })
-    }
+        });
+    };
+
+    const removeFromCart = (payload) => {
+        setState({
+            ...state,
+            cart: state.cart.filter(items => items.id !== payload.id),
+        });
+    };
+
     return {
-        state, AddToCart
+        state,
+        AddToCart,
+        removeFromCart
     }
 }
 
